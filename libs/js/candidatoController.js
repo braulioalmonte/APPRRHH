117bb4abe346149f86aad8d991b4d16b8bb57f93
braulioalmonte <brauliox2112@hotmail.com>
2022-10-12 10:08:51 -0400

    Busqueda de idiomas y candidatos por criterios

--- libs/js/candidatoController.js
+++ libs/js/candidatoController.js
@@ -255,25 +255,7 @@ $("#cedula").focusout(function(){
 	}
 
 })
-$(document).ready(function(){
-	$("#loginUsuario").click(function(){
-	
-		var usuario = $("#usuario").val();
-		var password = $("#password").val();
-	
-		db.transaction(function(transaction){
-			var sql="SELECT * FROM usuarios where usuario='"+usuario+"' AND password='"+password+"';";
-			transaction.executeSql(sql,undefined,function(transaction,result){
-				if(result.rows.length){
-					window.location.href = 'vistaIdiomas.html';
-				}
-				else{
-					alert("Datos no válidos, por favor verificar");
-				}
-			})
-		})
-	});
-})
+
 
 function verificarRolUsuario(){
 
@@ -293,6 +275,57 @@ function verificarRolUsuario(){
 window.addEventListener("load",verificarRolUsuario);
 window.addEventListener("load", cargarDatos);
 window.addEventListener("load", cargarOpciones);
+
+$("#buscarCandidato").change(function(){
+	var valor=$("#buscarCandidato").val();
+
+	$("#listaCandidatos").children().remove();
+	db.transaction(function(transaction){
+		var sql="SELECT * FROM candidatos where cedula like '%"+valor+
+		"%' OR nombre like '%"+valor+
+		"%' OR puesto like '%"+valor+
+		"%' OR departamento like '%"+valor+
+		"%' OR salario like '%"+valor+
+		"%' OR competencias like '%"+valor+
+		"%' OR capacitaciones like '%"+valor+
+		"%' OR experiencias like '%"+valor+
+		"%' OR recomendacion like '%"+valor+"%' ORDER BY id;";
+		transaction.executeSql(sql,undefined, function(transaction,result){
+			if(result.rows.length){
+				$("#listaCandidatos").append('<tr><th>ID</th><th>Cedula</th><th>Nombre</th><th>Puesto</th><th>Departamento</th><th>Salario que aspira</th><th>Competencias</th><th>Capacitaciones</th><th>Experiencias</th><th>Recomendado por</th><th></th><th></th><th></th></tr>');
+				for(var i=0; i<result.rows.length; i++){
+					var row=result.rows.item(i);
+					var cedula=row.cedula;
+					var nombre=row.nombre;
+					var puesto=row.puesto;
+					var departamento=row.departamento;
+					var salario=row.salario;
+					var competencias=row.competencias;
+					var capacitaciones=row.capacitaciones;
+					var experiencias=row.experiencias;
+					var recomendacion=row.recomendacion;
+					var id =row.id;
+					$("#listaCandidatos").append('<tr id="fila'+id+'" class="Reg_A'+id+'"><td><span class="mid">A'+
+					id+'</span></td><td><span>'+
+					cedula+'</span></td><td><span>'+
+					nombre+'</span></td><td><span>'+
+					puesto+'</span></td><td><span>'+
+					departamento+'</span></td><td><span>'+
+					salario+'</span></td><td><span>'+
+					competencias+'</span></td><td><span>'+
+					capacitaciones+'</span></td><td><span>'+
+					experiencias+'</span></td><td><span>'+
+					recomendacion+'</span></td><td><button type="button" id="A'+id+'" class="btn btn-warning" onclick="convertirEmpleado()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg></td><td><button type="button" id="A'+id+'" class="btn btn-success rrhh" onclick="editar()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5 2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg></button></td><td><button type="button" id="A'+id+'" class="btn btn-danger rrhh" onclick="eliminarRegistro()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 -1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg></button></td></tr>');
+				}
+			}else{
+				$("#listaCandidatos").append('<tr><td colspan="5" align="center">No existen registros de candidatos</td></tr>');
+			}
+			},function(transaction, err){
+				alert(err.message);
+			})
+		})
+
+})
 
 //Funcion para listar y pintar tabla de candidatos en la página web
 function cargarDatos(){
--- libs/js/idiomaController.js
+++ libs/js/idiomaController.js
@@ -26,7 +26,7 @@ function eliminarRegistro(){
 		db.transaction(function(transaction){
 			var sql="DELETE FROM idiomas WHERE id="+nuevoId+";"
 			transaction.executeSql(sql,undefined,function(){
-				alert("Registro borrado satisfactoriamente, Por favor actualice la tabla")
+				
 				window.location.reload();
 			}, function(transaction, err){
 				alert(err.message);
@@ -115,8 +115,35 @@ function verificarRolUsuario(){
 }
 
 window.addEventListener("load",verificarRolUsuario);
-
 window.addEventListener("load", cargarDatos);
+
+$("#buscarIdioma").change(function(){
+	var idioma = $("#buscarIdioma").val();
+	var estado = $("#buscarIdioma").val();
+
+	$("#listaIdiomas").children().remove();
+	db.transaction(function(transaction){
+		var sql="SELECT * FROM idiomas where idioma like '"+idioma+"%' OR estado='"+estado+"'ORDER BY id";
+		transaction.executeSql(sql,undefined, function(transaction,result){
+			if(result.rows.length){
+				$("#listaIdiomas").append('<tr><th>ID</th><th>Idioma</th><th>Estado</th><th></th><th></th></tr>');
+				for(var i=0; i<result.rows.length; i++){
+					var row=result.rows.item(i);
+					var idioma=row.idioma;
+					var id =row.id;
+					var estado=row.estado;
+					$("#listaIdiomas").append('<tr id="fila'+id+'" class="Reg_A'+id+'"><td><span class="mid">A'+
+					id+'</span></td><td><span>'+idioma+'</span></td><td><span>'+
+					estado+'</span></td><td><button type="button" id="A'+id+'" class="btn btn-success" onclick="editar()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5 2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg></button></td><td><button type="button" id="A'+id+'" class="btn btn-danger" onclick="eliminarRegistro()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 -1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg></button></td></tr>');
+				}
+			}else{
+				$("#listaIdiomas").append('<tr><td colspan="5" align="center">No existen registros de idiomas</td></tr>');
+			}
+			},function(transaction, err){
+				alert(err.message);
+			})
+		})
+})
 
 //Funcion para listar y pintar tabla de idiomas en la página web
 function cargarDatos(){
